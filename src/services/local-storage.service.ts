39ee59cc4186434b41e9e8665a518d9b283c07c5
Alexander Misyura <87127837+AlexanderMisyura@users.noreply.github.com>
2025-07-13 12:07:33 +0300

    feat: add local storage service

---
+++ src/services/local-storage.service.ts
@@ -0,0 +1,26 @@
+import config from '@config/app.config';
+
+class StorageService {
+  constructor(private storagePrefix = '') {}
+
+  public setItem(key: string, value: string): void {
+    const storageKey = this.getStorageKey(key);
+    localStorage.setItem(storageKey, JSON.stringify(value));
+  }
+
+  public getItem(key: string): string | null {
+    const storageKey = this.getStorageKey(key);
+    return localStorage.getItem(storageKey);
+  }
+
+  public removeItem(key: string): void {
+    const storageKey = this.getStorageKey(key);
+    localStorage.removeItem(storageKey);
+  }
+
+  private getStorageKey(key: string): string {
+    return `${this.storagePrefix}_${key}`;
+  }
+}
+
+export default new StorageService(config.STORAGE_PREFIX);
