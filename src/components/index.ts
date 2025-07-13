cacba13e24782c897f286415da60d22752debffa
Alexander Misyura <87127837+AlexanderMisyura@users.noreply.github.com>
2025-07-13 16:57:09 +0300

    feat: add heading component

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,3 +2,4 @@ export { BoxWrapper } from './BoxWrapper/BoxWrapper';
 export { Button } from './Button/Button';
 export { ErrorBoundary } from './ErrorBoundary/ErrorBoundary';
 export { ErrorFallback } from './ErrorFallback/ErrorFallback';
+export { Heading } from './Heading/Heading';
