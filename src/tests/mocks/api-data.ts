da2662c37994250ec2659752c2b856097c5063b8
Alexander Misyura <87127837+AlexanderMisyura@users.noreply.github.com>
2025-07-19 00:52:11 +0300

    feat: add mock api data for tests

---
+++ src/tests/mocks/api-data.ts
@@ -0,0 +1,13 @@
+export const recipe = {
+  id: 1,
+  name: 'test_name',
+  image: 'test_image',
+  ingredients: ['test_ingredient-1', 'test_ingredient-2', 'test_ingredient-3'],
+};
+
+export const recipesResponse = {
+  recipes: [recipe],
+  skip: 0,
+  total: 1,
+  limit: 1,
+};
---
+++ src/tests/mocks/index.ts
@@ -0,0 +1,2 @@
+export { recipe } from './api-data';
+export { recipesResponse } from './api-data';
