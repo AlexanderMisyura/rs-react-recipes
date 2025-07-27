e4410a856bb575eec2fe9d596c6ae9416d9aa51a
Alexander Misyura <87127837+AlexanderMisyura@users.noreply.github.com>
2025-07-27 14:20:04 +0300

    refactor: update header link styles

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -5,9 +5,10 @@ import { NavLink } from 'react-router';
 
 export const Header: React.FC = () => {
   return (
-    <header className="sticky top-0 flex w-full justify-center">
+    <header className="sticky top-0 z-10 flex w-full justify-center">
       <BoxWrapper className="m-0 w-full flex-row flex-wrap justify-between gap-4 p-2 max-sm:justify-center">
         <NavLink
+          className="transition-colors hover:text-orange-950"
           style={({ isActive }) => ({
             fontWeight: isActive ? 'bold' : 'inherit',
           })}
@@ -15,7 +16,10 @@ export const Header: React.FC = () => {
         >
           <div className="flex items-center gap-2 select-none">
             <img src={logo} alt="logo" className="h-12" />
-            <span style={{ fontWeight: 'inherit' }} className="text-2xl text-orange-900">
+            <span
+              style={{ fontWeight: 'inherit' }}
+              className="text-2xl text-orange-900 transition-colors in-[a:hover]:text-orange-950"
+            >
               Hot Recipes
             </span>
           </div>
@@ -24,6 +28,7 @@ export const Header: React.FC = () => {
           <ul>
             <li className="flex flex-col text-xl text-orange-900">
               <NavLink
+                className="transition-colors hover:text-orange-950"
                 to={UrlPath.ABOUT}
                 style={({ isActive }) => ({
                   fontWeight: isActive ? 'bold' : 'inherit',
