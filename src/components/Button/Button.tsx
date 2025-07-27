2f5e4849151c09046b83aaaf02629756052c8d15
Alexander Misyura <87127837+AlexanderMisyura@users.noreply.github.com>
2025-07-27 03:31:20 +0300

    refactor: update button component to hold a link

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,9 +1,12 @@
 import { clsx } from 'clsx/lite';
+import { Link } from 'react-router';
 import { twMerge } from 'tailwind-merge';
 
 interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   onClickHandler?: () => void;
   testId?: string;
+  linkTo?: string;
+  disabled?: boolean;
 }
 
 export const Button: React.FC<ButtonProps> = ({
@@ -12,17 +15,27 @@ export const Button: React.FC<ButtonProps> = ({
   onClickHandler,
   className,
   testId,
+  linkTo,
+  disabled,
 }) => {
   const combinedClasses = twMerge(
     clsx(
       'px-4 py-2',
       'cursor-pointer rounded-md border-2 border-transparent bg-sky-200 font-bold shadow-sm tracking-wide',
-      'hover:border-sky-300',
       'active:border-sky-200 active:bg-sky-100 active:text-gray-500 active:shadow-inner',
+      disabled ? 'pointer-events-none opacity-50' : 'hover:border-sky-300',
       'transition-colors',
       className
     )
   );
+
+  if (linkTo) {
+    return (
+      <Link data-testid={testId} to={linkTo} className={combinedClasses}>
+        {children}
+      </Link>
+    );
+  }
 
   return (
     <button
@@ -30,6 +43,7 @@ export const Button: React.FC<ButtonProps> = ({
       type={type ?? 'button'}
       className={combinedClasses}
       onClick={onClickHandler}
+      disabled={disabled}
     >
       {children}
     </button>
