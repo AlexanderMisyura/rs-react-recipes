1739f3611ff4f11bd31a6e16df1ded2dcd9a262a
Alexander Misyura <87127837+AlexanderMisyura@users.noreply.github.com>
2025-07-27 22:34:40 +0300

    feat: add api method for fetching details data

--- src/controllers/api.controller.ts
+++ src/controllers/api.controller.ts
@@ -1,6 +1,10 @@
 import config from '@config/api.config';
-import { recipesErrorSchema, recipesResponseSchema } from '@schemas';
-import type { RecipesResponse } from '@ts-types';
+import {
+  recipeInstructionsResponseSchema,
+  recipesErrorSchema,
+  recipesResponseSchema,
+} from '@schemas';
+import type { RecipeInstructionsResponse, RecipesResponse } from '@ts-types';
 
 export class ApiController {
   constructor(
@@ -10,7 +14,7 @@ export class ApiController {
 
   public async getItems(params: Record<string, string>): Promise<RecipesResponse> {
     const combinedParams = this.getCombinedParams(params);
-    const response = await fetch(`${this.apiUrl}?${combinedParams.toString()}`);
+    const response = await fetch(`${this.apiUrl}/search?${combinedParams.toString()}`);
 
     if (!response.ok) {
       const data = recipesErrorSchema.parse(await response.json());
@@ -19,6 +23,21 @@ export class ApiController {
 
     const recipesData = recipesResponseSchema.parse(await response.json());
     return recipesData;
+  }
+
+  public async getDetails(recipeId: string): Promise<RecipeInstructionsResponse> {
+    const params = new URLSearchParams();
+    params.set('select', 'instructions,name');
+
+    const response = await fetch(`${this.apiUrl}/${recipeId}?${params.toString()}`);
+
+    if (!response.ok) {
+      const data = recipesErrorSchema.parse(await response.json());
+      throw new Error(data.message);
+    }
+
+    const recipeData = recipeInstructionsResponseSchema.parse(await response.json());
+    return recipeData;
   }
 
   private getCombinedParams(params: Record<string, string>): URLSearchParams {
