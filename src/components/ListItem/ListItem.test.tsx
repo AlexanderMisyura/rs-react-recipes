16118e360e285b3f96ed2fa21d4fb5e456dd3e6c
Alexander Misyura <87127837+AlexanderMisyura@users.noreply.github.com>
2025-07-27 23:30:59 +0300

    refactor: update list item to comply to side panel

--- src/components/ListItem/ListItem.test.tsx
+++ src/components/ListItem/ListItem.test.tsx
@@ -1,31 +1,37 @@
-import { ListItem } from '@components';
-import { render, screen } from '@testing-library/react';
-import { recipe_1 as recipe } from '@tests-mocks';
+import { apiController } from '@controllers';
+import { renderWithRouter } from '@test-utils';
+import { screen } from '@testing-library/react';
+import { recipe_1 as recipe, recipesResponseSingle } from '@tests-mocks';
+import { UrlPath } from '@ts-enums';
+import { routes } from 'router';
+
+beforeEach(() => {
+  vi.spyOn(apiController, 'getItems').mockResolvedValue(recipesResponseSingle);
+});
 
 describe('ListItem', () => {
-  it('should display a correct name', () => {
-    render(<ListItem recipe={recipe} />);
+  it('should display a correct name', async () => {
+    renderWithRouter(routes, [UrlPath.RECIPES]);
 
-    const name = screen.getByText(recipe.name);
+    const name = await screen.findByText(recipe.name);
 
     expect(name).toBeInTheDocument();
   });
 
-  it('should display a correct image', () => {
-    render(<ListItem recipe={recipe} />);
+  it('should display a correct image', async () => {
+    renderWithRouter(routes, [UrlPath.RECIPES]);
 
-    const image = screen.getByAltText(recipe.name);
+    const image = await screen.findByAltText(recipe.name);
 
     expect(image).toBeInTheDocument();
     expect(image).toHaveAttribute('src', recipe.image);
     expect(image).toHaveAttribute('alt', recipe.name);
   });
 
-  it('should display correct ingredients', () => {
-    render(<ListItem recipe={recipe} />);
+  it('should display correct ingredients', async () => {
+    renderWithRouter(routes, [UrlPath.RECIPES]);
 
-    const ingredients = screen.getAllByRole('listitem');
-
+    const ingredients = await screen.findAllByTestId('ingredient');
     expect(ingredients).toHaveLength(recipe.ingredients.length);
     ingredients.forEach((ingredient, index) => {
       expect(ingredient).toHaveTextContent(recipe.ingredients[index]);
