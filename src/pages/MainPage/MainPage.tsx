8a656fa99fb401e5a7847159febba7e796ff74bc
Alexander Misyura <87127837+AlexanderMisyura@users.noreply.github.com>
2025-07-28 01:22:48 +0300

    fix: update main component
    Spinner on the main page now depends on search params changes.

--- src/pages/MainPage/MainPage.tsx
+++ src/pages/MainPage/MainPage.tsx
@@ -1,28 +1,30 @@
 import { BoxWrapper, Heading, List, Pagination, Search, Spinner } from '@components';
 import type { RecipesResponse } from '@ts-types';
-import { useLoaderData, useNavigation } from 'react-router';
+import { useLoaderData, useLocation, useNavigation } from 'react-router';
 
 export const MainPage = () => {
   const recipesData = useLoaderData<RecipesResponse>();
   const navigation = useNavigation();
+  const location = useLocation();
 
-  const loading = navigation.state === 'loading';
+  const isListLoading =
+    navigation.state === 'loading' && navigation.location.search !== location.search;
 
   return (
     <>
       <Search />
 
       <div data-testid="main-page" className="flex grow flex-col items-center justify-center gap-4">
-        {loading && <Spinner />}
+        {isListLoading && <Spinner />}
 
-        {!loading && recipesData.recipes.length > 0 && (
+        {!isListLoading && recipesData.recipes.length > 0 && (
           <>
             <List recipesData={recipesData} />
             <Pagination total={recipesData.total} />
           </>
         )}
 
-        {!loading && recipesData.recipes.length === 0 && (
+        {!isListLoading && recipesData.recipes.length === 0 && (
           <BoxWrapper testId="empty-fallback" className="border-2 border-orange-900">
             <Heading>Sorry, No Hot Recipes Found</Heading>
             <p className="text-xl">Try searching for something else</p>
