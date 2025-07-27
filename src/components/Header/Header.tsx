251d6db9d4a64865dea8baccb56e8bf20d76ed1a
Alexander Misyura <87127837+AlexanderMisyura@users.noreply.github.com>
2025-07-27 03:29:52 +0300

    refactor: update header logo link to change styles

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,18 +1,25 @@
 import logo from '@assets/logo.png';
 import { UrlPath } from '@ts-enums';
 import { BoxWrapper } from 'components/BoxWrapper/BoxWrapper';
-import { Link, NavLink } from 'react-router';
+import { NavLink } from 'react-router';
 
 export const Header: React.FC = () => {
   return (
     <header className="sticky top-0 flex w-full justify-center">
       <BoxWrapper className="m-0 w-full flex-row flex-wrap justify-between gap-4 p-2 max-sm:justify-center">
-        <Link to={UrlPath.HOME}>
+        <NavLink
+          style={({ isActive }) => ({
+            fontWeight: isActive ? 'bold' : 'inherit',
+          })}
+          to={UrlPath.RECIPES}
+        >
           <div className="flex items-center gap-2 select-none">
             <img src={logo} alt="logo" className="h-12" />
-            <span className="text-2xl font-bold text-orange-900">Hot Recipes</span>
+            <span style={{ fontWeight: 'inherit' }} className="text-2xl text-orange-900">
+              Hot Recipes
+            </span>
           </div>
-        </Link>
+        </NavLink>
         <nav className="p-4 text-center">
           <ul>
             <li className="flex flex-col text-xl text-orange-900">
