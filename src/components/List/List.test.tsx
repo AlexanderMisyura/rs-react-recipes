1ab6c6ee7267032858bd2fe091c4e15c82a15e6b
Alexander Misyura <87127837+AlexanderMisyura@users.noreply.github.com>
2025-07-28 01:19:02 +0300

    test: add list spinner test

--- src/components/List/List.test.tsx
+++ src/components/List/List.test.tsx
@@ -1,7 +1,7 @@
 import { apiController } from '@controllers';
 import { renderWithRouter } from '@test-utils';
 import { screen } from '@testing-library/react';
-import { recipesResponse, recipesResponseSingle } from '@tests-mocks';
+import { instructionsResponse, recipesResponse, recipesResponseSingle } from '@tests-mocks';
 import { UrlPath } from '@ts-enums';
 import { routes } from 'router';
 
@@ -34,5 +34,13 @@ describe('List', () => {
 
     const listItems = await screen.findAllByTestId('list-item', { exact: false });
     expect(listItems).toHaveLength(recipesResponse.recipes.length);
+  });
+
+  it('should display the spinner after the detailsId appear in the url', () => {
+    vi.spyOn(apiController, 'getDetails').mockResolvedValue(instructionsResponse);
+    renderWithRouter(routes, [`${UrlPath.RECIPES}/1/`]);
+
+    const spinner = screen.getByText('Loading...');
+    expect(spinner).toBeInTheDocument();
   });
 });
