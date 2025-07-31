3ec456c67d476dbdbf66d31880ec195ba8a1fdb5
Alexander Misyura <87127837+AlexanderMisyura@users.noreply.github.com>
2025-07-31 20:05:08 +0300

    feat: add mocked matchMedia window property

--- src/setup-tests.ts
+++ src/setup-tests.ts
@@ -3,4 +3,18 @@ import { expect } from 'vitest';
 
 window.scrollTo = vi.fn();
 
+beforeAll(() => {
+  Object.defineProperty(window, 'matchMedia', {
+    writable: true,
+    value: (query: string) => ({
+      matches: false,
+      media: query,
+      onchange: null,
+      addEventListener: vi.fn(),
+      removeEventListener: vi.fn(),
+      dispatchEvent: vi.fn(),
+    }),
+  });
+});
+
 expect.extend(matchers);
