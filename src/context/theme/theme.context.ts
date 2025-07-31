025c8483e4a3d085fbe66631fba6307803768efd
Alexander Misyura <87127837+AlexanderMisyura@users.noreply.github.com>
2025-07-31 19:49:45 +0300

    test: add tests form useThemeContext hook

--- src/context/theme/theme.context.ts
+++ src/context/theme/theme.context.ts
@@ -1,10 +1,4 @@
-import type { Theme, ThemeMode } from '@ts-types';
+import type { ThemeContextValue } from '@ts-interfaces';
 import { createContext } from 'react';
-
-export interface ThemeContextValue {
-  mode: ThemeMode;
-  theme: Theme;
-  toggleMode: () => void;
-}
 
 export const ThemeContext = createContext<ThemeContextValue | null>(null);
