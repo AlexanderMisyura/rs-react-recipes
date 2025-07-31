e08c34ff35a3e5d284aba96cd7d1cd46f195d46b
Alexander Misyura <87127837+AlexanderMisyura@users.noreply.github.com>
2025-07-31 18:18:25 +0300

    test: update test utils with wrapper parameter
    Able to wrap testing component with wrapper components, eg. context providers.

--- src/tests/utils/render-router.tsx
+++ src/tests/utils/render-router.tsx
@@ -2,16 +2,24 @@ import { render } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import { createMemoryRouter, type RouteObject, RouterProvider } from 'react-router';
 
-export const renderWithRouter = (routes: RouteObject[], initialEntries: string[]) => {
+interface RenderWithRouterProps {
+  routes: RouteObject[];
+  initialEntries: string[];
+  wrapper?: React.JSXElementConstructor<{
+    children: React.ReactNode;
+  }>;
+}
+
+export const renderWithRouter = ({ routes, initialEntries, wrapper }: RenderWithRouterProps) => {
   const router = createMemoryRouter(routes, { initialEntries });
-  const renderResult = render(<RouterProvider router={router} />);
+  const renderResult = render(<RouterProvider router={router} />, { wrapper });
 
   return { ...renderResult, router };
 };
 
-export const setupUserWithRouter = (routes: RouteObject[], initialEntries: string[]) => {
+export const setupUserWithRouter = ({ routes, initialEntries, wrapper }: RenderWithRouterProps) => {
   return {
     user: userEvent.setup(),
-    ...renderWithRouter(routes, initialEntries),
+    ...renderWithRouter({ routes, initialEntries, wrapper }),
   };
 };
