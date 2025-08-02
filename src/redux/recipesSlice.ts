8bad6257aa67782539ae9655be09d6f00d576c9f
Alexander Misyura <87127837+AlexanderMisyura@users.noreply.github.com>
2025-08-02 20:05:07 +0300

    feat: add redux

---
+++ src/redux/recipesSlice.ts
@@ -0,0 +1,38 @@
+import type { PayloadAction } from '@reduxjs/toolkit';
+import { createSlice } from '@reduxjs/toolkit';
+import type { Recipe } from '@ts-types';
+
+import type { RootState } from './store';
+
+export interface RecipesState {
+  recipesChecked: Recipe[];
+}
+
+const initialState: RecipesState = {
+  recipesChecked: [],
+};
+
+export const recipesSlice = createSlice({
+  name: 'recipes_checked',
+  initialState,
+  reducers: {
+    add: (state, action: PayloadAction<Recipe>) => {
+      state.recipesChecked.push(action.payload);
+    },
+    remove: (state, action: PayloadAction<number>) => {
+      const id = action.payload;
+      state.recipesChecked = state.recipesChecked.filter((recipe) => recipe.id !== id);
+    },
+    wipe: (state) => {
+      state.recipesChecked = [];
+    },
+  },
+});
+
+export const { add, remove, wipe } = recipesSlice.actions;
+
+export default recipesSlice.reducer;
+
+export const selectCheckedTotal = (state: RootState) => state.recipes.recipesChecked.length;
+
+export const selectAllChecked = (state: RootState) => state.recipes.recipesChecked;
