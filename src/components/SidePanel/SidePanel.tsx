9017a19c4c52742b34d191ac6913b000162e7df4
Alexander Misyura <87127837+AlexanderMisyura@users.noreply.github.com>
2025-08-14 01:51:09 +0300

    refactor: replace ternary rendering in SidePanel with conditionally returning JSX

--- src/components/SidePanel/SidePanel.tsx
+++ src/components/SidePanel/SidePanel.tsx
@@ -14,51 +14,53 @@ export const SidePanel: React.FC = () => {
 
   const closeDetailsUrl = `${UrlPath.RECIPES}?${searchParams}`;
 
-  return (
-    <>
-      {isFetching ? (
-        <Spinner />
-      ) : isError ? (
-        <ErrorFallback
-          title={'status' in error ? error.status.toString() : 'Error'}
-          error={
-            new Error('data' in error ? String(error.data) : 'Something went wrong with the data')
-          }
-          btnChildren="Close"
-          resetFunction={() => void navigate(closeDetailsUrl)}
-        />
-      ) : (
-        data && (
-          <BoxWrapper
-            testId="side-panel"
-            className={clsx(`${theme}-text`, 'flex w-full flex-col gap-4')}
-          >
-            <h3 className="text-center font-bold text-balance text-orange-900">
-              Instructions for cooking {data.name}
-            </h3>
-            <ul className="flex list-disc flex-col gap-1 pl-6 text-sm">
-              {data.instructions.map((instruction) => {
-                return (
-                  <li data-testid="instruction" key={instruction}>
-                    {instruction}
-                  </li>
-                );
-              })}
-            </ul>
-            <Button
-              testId="refetch-details"
-              onClickHandler={() => void refetch()}
-              disabled={isFetching}
-              className="w-full"
-            >
-              Refetch
-            </Button>
-            <Button className="w-full" linkTo={closeDetailsUrl}>
-              Close
-            </Button>
-          </BoxWrapper>
-        )
-      )}
-    </>
-  );
+  if (isFetching) {
+    return <Spinner />;
+  }
+
+  if (isError) {
+    return (
+      <ErrorFallback
+        title={'status' in error ? error.status.toString() : 'Error'}
+        error={
+          new Error('data' in error ? String(error.data) : 'Something went wrong with the data')
+        }
+        btnChildren="Close"
+        resetFunction={() => void navigate(closeDetailsUrl)}
+      />
+    );
+  }
+
+  if (data) {
+    return (
+      <BoxWrapper
+        testId="side-panel"
+        className={clsx(`${theme}-text`, 'flex w-full flex-col gap-4')}
+      >
+        <h3 className="text-center font-bold text-balance text-orange-900">
+          Instructions for cooking {data.name}
+        </h3>
+        <ul className="flex list-disc flex-col gap-1 pl-6 text-sm">
+          {data.instructions.map((instruction) => {
+            return (
+              <li data-testid="instruction" key={instruction}>
+                {instruction}
+              </li>
+            );
+          })}
+        </ul>
+        <Button
+          testId="refetch-details"
+          onClickHandler={() => void refetch()}
+          disabled={isFetching}
+          className="w-full"
+        >
+          Refetch
+        </Button>
+        <Button className="w-full" linkTo={closeDetailsUrl}>
+          Close
+        </Button>
+      </BoxWrapper>
+    );
+  }
 };
