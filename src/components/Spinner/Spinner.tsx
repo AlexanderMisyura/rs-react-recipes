722f9421fc43e10557b9e78cd2a06771d830f768
Alexander Misyura <87127837+AlexanderMisyura@users.noreply.github.com>
2025-08-17 17:16:40 +0300

    feat: add loading route fallback

--- src/components/Spinner/Spinner.tsx
+++ src/components/Spinner/Spinner.tsx
@@ -1,11 +1,11 @@
-import chiliIcon from '@assets/chili.svg';
+import ChiliIcon from '@assets/chili.svg';
 import { Heading } from '@components';
 
 export const Spinner: React.FC = () => {
   return (
     <div>
       <div className="h-40 w-40 animate-spin">
-        <img src={chiliIcon} alt="chili pepper loading spinner" />
+        <ChiliIcon width={160} height={160} alt="chili pepper loading spinner" />
       </div>
       <Heading>Loading...</Heading>
     </div>
