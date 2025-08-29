9fe09cd876f2b63d430ff9a5e1e68feb3aaee7f7
Alexander Misyura <87127837+AlexanderMisyura@users.noreply.github.com>
2025-08-29 20:38:25 +0300

    refactor: update year selector
    Used select control instead of dropdown

--- src/components/controls/year-selector/year-selector.tsx
+++ src/components/controls/year-selector/year-selector.tsx
@@ -1,7 +1,5 @@
 import type { DatasetStored } from '@ts-types';
-import { clsx } from 'clsx';
-import { use, useEffect, useRef, useState } from 'react';
-import { FaAngleDown, FaAngleUp } from 'react-icons/fa';
+import { use, useRef } from 'react';
 
 interface YearSelectorProps {
   datasetPromise: Promise<DatasetStored>;
@@ -14,50 +12,27 @@ export const YearSelector: React.FC<YearSelectorProps> = ({
   updateYearFilter,
   year,
 }) => {
-  const [isActive, setIsActive] = useState<boolean>(false);
-  const yearSelectorRef = useRef<HTMLDivElement>(null);
   const yearsRef = useRef(new Set<number>());
 
   use(datasetPromise).forEach(({ data }) => {
     data.forEach(({ year }) => yearsRef.current.add(year));
   });
 
-  useEffect(() => {
-    setIsActive(false);
-  }, [year]);
-
   return (
-    <div ref={yearSelectorRef} className={clsx(isActive && 'is-active', 'dropdown')}>
-      <div className="dropdown-trigger">
-        <button
-          onClick={() => {
-            setIsActive(!isActive);
-          }}
-          type="button"
-          className="button w-[135px]"
-          aria-haspopup="true"
-          aria-controls="dropdown-menu"
-        >
-          <span>Year - {year}</span>
-          <span className="icon">{isActive ? <FaAngleUp /> : <FaAngleDown />}</span>
-        </button>
-      </div>
-      <div className="dropdown-menu min-w-[135px]!" id="dropdown-menu" role="menu">
-        <div className="dropdown-content max-h-[300px] overflow-y-auto">
-          {[...yearsRef.current].reverse().map((year) => (
-            <button
-              onClick={() => {
-                updateYearFilter(year);
-              }}
-              type="button"
-              key={year}
-              className="dropdown-item has-text-centered pr-2"
-            >
-              {year}
-            </button>
-          ))}
-        </div>
-      </div>
+    <div className="select">
+      <select
+        value={year}
+        name="year"
+        onChange={(e) => {
+          updateYearFilter(+e.target.value);
+        }}
+      >
+        {[...yearsRef.current].reverse().map((year) => (
+          <option key={year} value={year}>
+            {year}
+          </option>
+        ))}
+      </select>
     </div>
   );
 };
